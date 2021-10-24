4037be2efac83d4608c00f084221e70931d50f93
Sayyed Abrar Akhtar <akhtars10@uni.coventry.ac.uk>
2021-10-24 22:37:44 +0545

    Updated contents

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -4,7 +4,7 @@ export const educations = [
     university: 'Tribhuvan University',
     college: 'Central Department of Geology, Kirtipur',
     year: '2017',
-    score: '3.49 CGPA, First Division',
+
     thesis:
       'Geological, Geophysical and Hydrogeological Characteristics of Gokarna area and its surrounding',
   },
@@ -14,20 +14,17 @@ export const educations = [
     university: 'Tribhuvan University, ',
     college: 'Trichandra Campus, Ghantaghar, Kathmandu',
     year: '2012',
-    score: '62.57%, First Division',
   },
   {
     major: 'Intermediate (10+2) in Science (Bio Group with Extra Math’s)',
     university: 'HSEB Board',
     college: 'National Integrated College, Dillibazar, Kathmandu',
     year: '2008',
-    score: '60.67% First Division',
   },
   {
     major: 'School Leaving Certificate',
     university: 'HMG Board',
     college: 'Neptune Boarding High School, Bagbazar, Kathmandu',
     year: '2005',
-    score: '74%, First Division',
   },
 ];
--- src/data/otherActivitiesData.js
+++ src/data/otherActivitiesData.js
@@ -1,6 +1,7 @@
 export const otherActivities = [
   'Facilitator for “Training on Best Practices in Deep Tube Well Drilling in Nepal” for Nepal Water Supply Corporation (NWSC) branch manager and technical personnel of Water Aid Nepal and Dalit Jan Kalyan Yuwa Club, (3-6, March, 2021, Lahan, Siraha District)',
   'Participation as a deligate in the live webinar on “Mapping the Future with GIS” Organized by Division of Geoinformatics, Faculty of Natural Sciences JSS Academy of Higher Education & Research, Mysuru on Thursday, 19th November,2020.',
+  'Member of Scientific Committee of Nepal Hydrogeological Association (2019-2021)',
   'Member of 5th Executive Committee of Nepal Hydrogeological Association (2019-2021)',
   ' Member of International Association of Engineering Geology and Environment (IAEG)',
   'Life Member of Nepal Hydrogeological Association (Membership No: LM-100)',
